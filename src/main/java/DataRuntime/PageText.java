c71f55bbd5dac6b72680fdf27905fb4e7134873e
Arif <Arif@DESKTOP-T4EC9E6>
2017-12-28 21:59:42 +0500

    shell

---
+++ src/main/java/DataRuntime/PageText.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataRuntime;
+
+import java.util.List;
+
+/**
+ *
+ * @author Arif
+ */
+public class PageText {
+    
+    List<String> headings1;
+    List<String> headings2;    
+    List<String> headings3;
+    List<Word> words;
+    List<Word> links;
+  
+    
+    
+    
+}
