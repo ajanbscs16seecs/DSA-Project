149e81ec0a22fc8c6b9c06ae11d542b0e58ce7eb
Executioner_rip <hjaved.bscs16seecs@seecs.edu.pk>
2017-12-30 00:24:38 -0800

    Done

--- src/xml/parsing/XmlParsing.java
+++ src/xml/parsing/XmlParsing.java
@@ -5,6 +5,7 @@
  */
 package xml.parsing;
 
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Element;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,6 +13,9 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
@@ -28,6 +32,27 @@ public class XmlParsing {
         try {
             DocumentBuilder builder = factory.newDocumentBuilder();
            Document doc  = builder.parse("books.xml");
+           NodeList book_list = doc.getElementsByTagName("book");
+           for (int i=0;i<book_list.getLength();i++)
+           {
+               Node b = book_list.item(i);
+               if (b.getNodeType()== Node.ELEMENT_NODE)
+               {
+                   Element books = (Element) b;
+                   String id  = books.getAttribute("id");
+                  NodeList child_names = books.getChildNodes();
+                  for (int j=0;j<child_names.getLength();j++)
+                  {
+                      Node n = child_names.item(j);
+                      if (n.getNodeType() == Node.ELEMENT_NODE)
+                      {
+                        Element name = (Element) n;
+                        System.out.println("Book : " + id + ":" + name.getTagName()+ "=" + name.getTextContent());
+                      }
+                  }
+                   
+               }
+           }
         } catch (ParserConfigurationException ex) {
             Logger.getLogger(XmlParsing.class.getName()).log(Level.SEVERE, null, ex);
         }
