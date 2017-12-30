a01081f5d8a77e616053a513d3539c54c3d74924
Executioner_rip <hjaved.bscs16seecs@seecs.edu.pk>
2017-12-29 20:38:01 -0800

    Document Builder

--- src/xml/parsing/XmlParsing.java
+++ src/xml/parsing/XmlParsing.java
@@ -5,6 +5,8 @@
  */
 package xml.parsing;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+
 /**
  *
  * @author Hassan Javed
@@ -15,7 +17,7 @@ public class XmlParsing {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        DocumentBuilderFactory factory;
     }
     
 }
