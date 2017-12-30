0428e8636fc4cf6213abf151f4a3d6a58a9f899c
Executioner_rip <hjaved.bscs16seecs@seecs.edu.pk>
2017-12-29 20:55:49 -0800

    So Far

--- src/xml/parsing/XmlParsing.java
+++ src/xml/parsing/XmlParsing.java
@@ -5,7 +5,14 @@
  */
 package xml.parsing;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -16,8 +23,14 @@ public class XmlParsing {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        DocumentBuilderFactory factory;
+    public static void main(String[] args) throws SAXException, IOException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance(); //This is a static method which returns the document builder method
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+           Document doc  = builder.parse("books.xml");
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(XmlParsing.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
